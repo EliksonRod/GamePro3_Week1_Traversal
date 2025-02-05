e16a6d6840ba1b9bb4a24a0002c1b24fb3517bd0
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2025-02-05 13:52:33 -0500

    Expanded level, added working turrets, added some images

--- TraversalGame_GamePro3/Assets/Scripts/bullet.cs
+++ TraversalGame_GamePro3/Assets/Scripts/bullet.cs
@@ -1,6 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class bullet
+public class bullet : MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
+    [SerializeField] private LayerMask whatDestroysBullet;
+
+    public float bulletSpeed = 15f;
+    public float destroyTime = 5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        SetDestroyTime();
+
+        InitializeBullet();
+    }
+
+    private void InitializeBullet()
+    {
+        rb.linearVelocity = transform.right * bulletSpeed;
+    }
+      
+    private void SetDestroyTime()
+    {
+    //destroy bullet after some time
+        Destroy(gameObject, destroyTime);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+    //destroy bullet after hitting another collider
+        Destroy(gameObject);
+    }
 }
--- TraversalGame_GamePro3/Assets/Scripts/turret.cs
+++ TraversalGame_GamePro3/Assets/Scripts/turret.cs
@@ -1,6 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class turret
+
+public class turret : MonoBehaviour
 {
-    
+    public float timer = 10f;
+    private float shootTimer;
+
+    public Rigidbody2D rb;
+    public GameObject bulletPrefab;
+    public Transform weaponBarrel;
+
+    void Start()
+    {
+        shootTimer = timer;
+    }
+    void FixedUpdate()
+    {
+        shootTimer--; 
+
+        if (shootTimer <= 0)
+        {
+            FireWeapon();
+            shootTimer = Random.Range(timer, 90f);
+        }
+    }
+    public void FireWeapon()
+    {
+        //shoots prefab, tracks position and rotation of the tip of the weapon
+        GameObject bullet = Instantiate(bulletPrefab, weaponBarrel.position, weaponBarrel.rotation);
+    }
 }
